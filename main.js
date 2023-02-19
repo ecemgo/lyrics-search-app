d6a64c37543b93c96907da44390a7668f19ee85c
Ecem Gokdogan <ecemgo@hotmail.com>
2023-02-19 18:06:38 +0200

    main.js updated

---
+++ main.js
@@ -0,0 +1,49 @@
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const result = document.getElementById("result");
+const more = document.getElementById("more");
+
+const apiURL = "https://api.lyrics.ovh";
+
+// Search by song or artist
+async function searchSongs(term) {
+  //   fetch(`${apiURL}/suggest/${term}`)
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data));
+
+  const res = await fetch(`${apiURL}/suggest/${term}`);
+  const data = await res.json();
+
+  //   console.log(data);
+  showData(data);
+}
+
+// Show song and artist in DOM
+function showData(data) {
+  let output = "";
+
+  data.data.forEach((song) => {
+    output += `
+        <li>
+            <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+            <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+        </li>
+        `;
+  });
+
+  result.innerHTML;
+}
+
+// Event Listeners
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value.trim();
+  // console.log(searchTerm);
+
+  if (!searchTerm) {
+    alert("Please type in a search term");
+  } else {
+    searchSongs(searchTerm);
+  }
+});
